eb3c0fab4c6ae4df8e6f2403da78e81e1b0b19ee
nchore <nchoreanthony@gmail.com>
2023-12-19 14:55:00 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
-def write_file(file_name, file_content):
-    pass
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
+def append_to_file(file_name, append_content):
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(f"\n{append_content}")
 
 def read_file(file_name):
-    pass
+    try:
+        with open(f"{file_name}.txt", "r") as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "File not found"
+
+# Example usage
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_to_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+content = read_file(file_name="logfile")
+print(content)
+
